4bf126c8dca7a48807d67f9366a6df36726b8f51
LuciusL <lucius.lechner@porsche.digital>
2025-03-31 18:02:53 +0200

    Game funktioniert. Fehlt noch gewinnen

--- source/main.py
+++ source/main.py
@@ -9,16 +9,58 @@ def main():
     """main funktion des Spiels"""
     playing_field = get_playing_field()
     numbers_field = get_field_numbers(playing_field)
+    # print_field_as_table(playing_field)
+    # print_field_as_table(numbers_field)
+    allowed_characters = {
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 3,
+        "e": 4,
+    }
+    allowed_numbers = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
     shown_field = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
     ]
-    print_field_as_table(shown_field)
+    print("Willkommen in SweeperMine. Deine Aufgabe ist es Felder aufzudecken.")
+    print("Decke Felder auf indem du die Koordinaten angibst: (A1, C5, ...)\n")
 
-    return
+    while True:
+        print_field_as_table(shown_field)
+
+        chosen_cell = input().lower()
+        row_numbers = [int(c) for c in list(chosen_cell) if c.isdigit()]
+        column_chars = [c for c in list(chosen_cell) if not c.isdigit()]
+        # print(column_chars, row_numbers)
+        try:
+            chosen_row = allowed_numbers[row_numbers[0]]
+            chosen_column = allowed_characters[column_chars[0]]
+            # print(chosen_column, chosen_row)
+            # print_field_as_table(numbers_field)
+            # print(numbers_field[chosen_row][chosen_column])
+            if numbers_field[chosen_row][chosen_column] == 9:
+                print("Verloren!")
+                print_field_as_table(playing_field)
+                playing_field = get_playing_field()
+                numbers_field = get_field_numbers(playing_field)
+                shown_field = [
+                    ["x", "x", "x", "x", "x"],
+                    ["x", "x", "x", "x", "x"],
+                    ["x", "x", "x", "x", "x"],
+                    ["x", "x", "x", "x", "x"],
+                    ["x", "x", "x", "x", "x"],
+                ]
+            else:
+                shown_field[chosen_row][chosen_column] = str(
+                    numbers_field[chosen_row][chosen_column]
+                )
+
+        except:
+            print("Ungültige Eingabe")
 
 
 if __name__ == "__main__":
--- source/print_field_as_table.py
+++ source/print_field_as_table.py
@@ -14,8 +14,12 @@ def print_field_as_table(field):
     print("   A B C D E\n")
 
     for i, row in enumerate(field):
-        str_row = map(str, row)
-        print(str(i + 1) + "  " + " ".join(str_row))
+        if type(row[0]) is int:
+            print_row = map(str, row)
+        else:
+            print_row = row
+        print(str(i + 1) + "  " + " ".join(print_row))
+    print("")
 
 
 # if __name__ == "__main__":
