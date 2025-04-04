f8385e303443c11e4ca9b741fe10692f0348bce7
LuciusL <lucius.lechner@porsche.digital>
2025-04-04 14:35:48 +0200

    Refactor win_condition function to improve readability and maintainability. What he said...

--- source/win_condition.py
+++ source/win_condition.py
@@ -1,5 +1,18 @@
 """Funktion, die das Gewinnen vom Spiel bestimmt"""
 
+from typing import List
+
+
+def win_condition(shown_field: List[List[str]], bomb_count: int) -> bool:
+    """Wenn nur noch 5 unaufdeckte Felder -> Spiel gewonnen"""
+    x_count = 0
+    for row in shown_field:
+        for cell in row:
+            if cell == "x":
+                x_count = x_count + 1
+    return x_count <= bomb_count
+
+
 # shown_field_1 = [
 #     ["x", "x", "x", "x", "x"],
 #     ["x", "x", "x", "x", "x"],
@@ -14,18 +27,6 @@
 #     ["b", "b", "b", "b", "b"],
 #     ["b", "b", "b", "b", "b"],
 # ]
-from typing import List
-
-
-def win_condition(shown_field: List[List[str]], bomb_count: int) -> bool:
-    """Wenn nur noch 5 unaufdeckte Felder -> Spiel gewonnen"""
-    x_count = 0
-    for row in shown_field:
-        for cell in row:
-            if cell == "x":
-                x_count = x_count + 1
-    return x_count <= bomb_count
-
 
 # if __name__ == "__main__":
 #     print(win_condition(shown_field_2))
